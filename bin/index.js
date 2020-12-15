34fe0f02b64206e8269e84d97ed4ad02e11063f9
lyrieek <lyrieek@qq.com>
2020-12-15 13:35:01 +0800

    :package:Arrange release eslint-th@0.0.7

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const dispose = require("../lib/dispose")
 const print = require("../lib/print")
 const options = require("../lib/options")
 
-const start_hrtime = process.hrtime();
+const start_hrtime = process.hrtime()
 const currentOptions = options.parse(process.argv)
 
 print(dispose(currentOptions), { all: currentOptions.all, start_time: Date.now(), start_hrtime })
